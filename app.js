d451de5dae39e21c8e67eacd7d54d67097b3050b
Félix Simões <felixssimoes@gmail.com>
2020-04-22 11:49:35 +0200

    remove alerts

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const cameraView = document.querySelector('#camera--view'),
 // Access the device camera and stream to cameraView
 function cameraStart() {
   webkit.messageHandlers.callbackHandler.postMessage('Camera start');
-  alert('Camera start');
   navigator.mediaDevices
     .getUserMedia(constraints)
     .then(function (stream) {
@@ -20,7 +19,6 @@ function cameraStart() {
       webkit.messageHandlers.callbackHandler.postMessage(
         'Oops. Something is broken.' + JSON.stringify(error)
       );
-      alert('Oops. Something is broken.' + JSON.stringify(error));
     });
 }
 // Take a picture when cameraTrigger is tapped
