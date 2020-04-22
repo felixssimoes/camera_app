2421de283ad5074049c4397dbf6ae18ec57bc660
Félix Simões <felixssimoes@gmail.com>
2020-04-22 12:29:31 +0200

    .

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-(function () {
-  if (!window.navigator) window.navigator = {};
-  window.navigator.getUserMedia = function () {
-    webkit.messageHandlers.callbackHandler.postMessage(arguments);
-  };
-})();
+// (function () {
+//   if (!window.navigator) window.navigator = {};
+//   window.navigator.getUserMedia = function () {
+//     webkit.messageHandlers.callbackHandler.postMessage(arguments);
+//   };
+// })();
 
-var exec_id = 0;
-function exec(success, failure, ...) {
-  // store the callbacks for later
-  if (typeof success == 'function' || typeof failure == 'function') {
-    exec_id++;
-    exec_callbacks[exec_id] = { success: success, failure: failure };
-    var commandId = exec_id;
-  }
-  webkit.messageHandlers.callbackHandler.postMessage({id: commandId, args: ...})
-}
+// var exec_id = 0;
+// function exec(success, failure, ...) {
+//   // store the callbacks for later
+//   if (typeof success == 'function' || typeof failure == 'function') {
+//     exec_id++;
+//     exec_callbacks[exec_id] = { success: success, failure: failure };
+//     var commandId = exec_id;
+//   }
+//   webkit.messageHandlers.callbackHandler.postMessage({id: commandId, args: ...})
+// }
 
-// the native code calls this directly with the same commandId, so the callbacks can be performed and released
-function callback(opts) {
-  if (opts.status == "success") {
-    if (typeof exec_callbacks[opts.id].success == 'function') exec_callbacks[opts.id].success(opts.args);
-  } else {
-    if (typeof exec_callbacks[opts.id].failure == 'function') exec_callbacks[opts.id].failure(opts.args);
-  }
-  // some WebRTC functions invoke the callbacks multiple times
-  // the native Cordova plugin uses setKeepCallbackAs(true)
-  if (!opts.keepalive) delete exec_callbacks[opts.id];
-}
+// // the native code calls this directly with the same commandId, so the callbacks can be performed and released
+// function callback(opts) {
+//   if (opts.status == "success") {
+//     if (typeof exec_callbacks[opts.id].success == 'function') exec_callbacks[opts.id].success(opts.args);
+//   } else {
+//     if (typeof exec_callbacks[opts.id].failure == 'function') exec_callbacks[opts.id].failure(opts.args);
+//   }
+//   // some WebRTC functions invoke the callbacks multiple times
+//   // the native Cordova plugin uses setKeepCallbackAs(true)
+//   if (!opts.keepalive) delete exec_callbacks[opts.id];
+// }
 
 // Set constraints for the video stream
 var constraints = { video: { facingMode: 'user' }, audio: false };
