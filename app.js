52c53d09097e6a2a535497b88331470c1525d565
Félix Simões <felixssimoes@gmail.com>
2020-04-22 11:41:07 +0200

    post message on getmedia error

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
+var constraints = { video: { facingMode: 'user' }, audio: false };
 // Define constants
-const cameraView = document.querySelector("#camera--view"),
-  cameraOutput = document.querySelector("#camera--output"),
-  cameraSensor = document.querySelector("#camera--sensor"),
-  cameraTrigger = document.querySelector("#camera--trigger");
+const cameraView = document.querySelector('#camera--view'),
+  cameraOutput = document.querySelector('#camera--output'),
+  cameraSensor = document.querySelector('#camera--sensor'),
+  cameraTrigger = document.querySelector('#camera--trigger');
 // Access the device camera and stream to cameraView
 function cameraStart() {
   navigator.mediaDevices
     .getUserMedia(constraints)
-    .then(function(stream) {
+    .then(function (stream) {
       track = stream.getTracks()[0];
       cameraView.srcObject = stream;
     })
-    .catch(function(error) {
-      console.error("Oops. Something is broken.", error);
+    .catch(function (error) {
+      console.error('Oops. Something is broken.', error);
+      webkit.messageHandlers.callbackHandler.postMessage(
+        'Oops. Something is broken.' + JSON.stringify(error)
+      );
     });
 }
 // Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
+cameraTrigger.onclick = function () {
   cameraSensor.width = cameraView.videoWidth;
   cameraSensor.height = cameraView.videoHeight;
-  cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-  cameraOutput.src = cameraSensor.toDataURL("image/webp");
-  cameraOutput.classList.add("taken");
+  cameraSensor.getContext('2d').drawImage(cameraView, 0, 0);
+  cameraOutput.src = cameraSensor.toDataURL('image/webp');
+  cameraOutput.classList.add('taken');
 };
 // Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
+window.addEventListener('load', cameraStart, false);
